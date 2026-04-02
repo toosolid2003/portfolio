4449c26e90db90eb8031ab6d0bc30910e41e1ae1
toosolid2003 <toosolid2003@yahoo.fr>
2026-04-02 16:49:11 +0200

    Removed preoptima and fonterra

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -14,8 +14,6 @@ const routes = {
 // Set password in the .env file, refer to .env.example
 const protectedRoutes = {
   "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
-  "/work/preoptima": true,
-  "/work/fonterra": true,
 };
 
 import { Geist } from "next/font/google";
